5e55a5ba4e57df27875dcbc5a2699a5cfeccad82
JungHwa Kang <pacino.kang@gmail.com>
2022-04-29 15:40:46 +0900

    messageLisner추가

--- src/main/java/com/kt/api/controller/RabbitmqController.java
+++ src/main/java/com/kt/api/controller/RabbitmqController.java
@@ -17,7 +17,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @RestController
@@ -32,32 +34,36 @@ public class RabbitmqController {
 
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
-        public ResponseEntity<MQResponse> getKubeletLog(@RequestBody @Valid MQRequest request) throws InterruptedException{
+        public ResponseEntity<MQResponse> getKubeletLog(@RequestBody @Valid MQRequest request) throws InterruptedException, IOException, TimeoutException {
         log.info("{}", String.format("'%s' message를 전송합니다.", request.getRequestMessage()));
 
         String routingKey=request.getRouteKey();
         String message=request.getRequestMessage();
 
-        try {
+        MQResponse response = new MQResponse();
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
 
             Channel channel = RabbitMQConfig.getChannel();
+        try {
+
             channel.exchangeDeclare(EXCHANGE_NAME, "topic");
 
             channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        MQResponse response = new MQResponse();
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
 
         response.setStatus(StatusEnum.OK);
         response.setMessage("Message Send Success!");
         response.setData("Message Send Success!");
         System.out.println("Message sent to the RabbitMQ Topic Exchange Successfully") ;
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            channel.close();
+        }
         return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
 }
