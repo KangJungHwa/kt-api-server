3c143ceb61b7eac694c2a3ef1462fea5a30384a4
JungHwa Kang <pacino.kang@gmail.com>
2022-04-29 23:55:58 +0900

    messageLisner추가

--- src/main/java/com/kt/api/config/RabbitMqConfiguration.java
+++ src/main/java/com/kt/api/config/RabbitMqConfiguration.java
@@ -1,21 +1,67 @@
 package com.kt.api.config;
 
-import com.kt.api.util.RabbitmqConnectionManager;
+
 import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 @Configuration
 public class RabbitMqConfiguration {
+    private static String host;
+    private static int port;
+    private static String username;
+    private static String password;
+
+    @Value("${spring.rabbitmq.host}")
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    @Value("${spring.rabbitmq.port}")
+    public void setPort(int port) {
+        this.port=port;
+    }
+    @Value("${spring.rabbitmq.username}")
+    public void setUsername(String username) {
+        this.username=username;
+    }
+    @Value("${spring.rabbitmq.password}")
+    public void setPassword(String password) {
+        this.password=password;
+    }
+
+    private static Connection connection = null;
+    @PostConstruct
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                ConnectionFactory connectionFactory = new ConnectionFactory();
+
+                connectionFactory.setHost(host);
+                connectionFactory.setPort(port);
+                connectionFactory.setUsername(username);
+                connectionFactory.setPassword(password);
+
+                connection = connectionFactory.newConnection();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 
     @Bean
     public  void declareExchange() throws IOException, TimeoutException {
-        Channel channel = RabbitmqConnectionManager.getConnection().createChannel();
+        Channel channel = getConnection().createChannel();
         //Create Topic Exchange
         channel.exchangeDeclare("nlu-topic-exchange", BuiltinExchangeType.TOPIC, true);
         channel.close();
@@ -23,7 +69,7 @@ public class RabbitMqConfiguration {
     @Bean
     public  void declareQueues() throws IOException, TimeoutException {
         //Create a channel - do not share the Channel instance
-        Channel channel = RabbitmqConnectionManager.getConnection().createChannel();
+        Channel channel = getConnection().createChannel();
         //Create the Queues
         channel.queueDeclare("intentTrain", true, false, false, null);
         channel.queueDeclare("intentService", true, false, false, null);
@@ -34,7 +80,7 @@ public class RabbitMqConfiguration {
 
     @Bean
     public  void declareBindings() throws IOException, TimeoutException {
-        Channel channel = RabbitmqConnectionManager.getConnection().createChannel();
+        Channel channel = getConnection().createChannel();
         //Create bindings - (queue, exchange, routingKey) - routingKey != null
         channel.queueBind("intentTrain", "nlu-topic-exchange", "intent.train.*");
         channel.queueBind("intentService", "nlu-topic-exchange", "intent.service.*");
--- src/main/java/com/kt/api/controller/RabbitmqController.java
+++ src/main/java/com/kt/api/controller/RabbitmqController.java
@@ -1,10 +1,11 @@
 package com.kt.api.controller;
 
 
+import com.kt.api.config.RabbitMqConfiguration;
 import com.kt.api.model.MQRequest;
 import com.kt.api.model.MQResponse;
 import com.kt.api.model.StatusEnum;
-import com.kt.api.util.RabbitmqConnectionManager;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -27,49 +28,14 @@ import java.util.concurrent.TimeoutException;
 @RequestMapping("/rabbitmq")
 public class RabbitmqController {
 
-
-//    @Value("${spring.rabbitmq.host}")
-//    private String host;
-//
-//    @Value("${spring.rabbitmq.port}")
-//    private int port;
-//
-//    @Value("${spring.rabbitmq.username}")
-//    private String username;
-//
-//    @Value("${spring.rabbitmq.password}")
-//    private String password;
-//
-//    @Value("${spring.rabbitmq.queue}")
-//    private String QUEUE_NAME;
-    //private static final String EXCHANGE_NAME = "topic_nlu";
-
-
     @RequestMapping(value = "/send", method = RequestMethod.POST)
         public ResponseEntity<MQResponse> sendMessage(@RequestBody @Valid MQRequest request) throws IOException, TimeoutException {
         log.info("{}", String.format("'%s' message를 전송합니다.", request.getRequestMessage()));
 
-        Channel channel = RabbitmqConnectionManager.getConnection().createChannel();
+        Channel channel = RabbitMqConfiguration.getConnection().createChannel();
         String message = "Drink a lot of Water and stay Healthy!";
         channel.basicPublish("nlu-topic-exchange", request.getRouteKey(), null, message.getBytes());
-//
-//        log.info("host : "+host);
-//        log.info("port : "+port);
-//        log.info("username : "+username);
-//        log.info("password : "+password);
-//        String routingKey=request.getRouteKey();
-//        String message=request.getRequestMessage();
-//        ConnectionFactory factory = new ConnectionFactory();
-//        factory.setHost(host);
-//        factory.setPort(port);
-//        factory.setUsername(username);
-//        factory.setPassword(password);
-//        try (Connection connection = factory.newConnection();
-//             Channel channel = connection.createChannel()) {
-//             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-//             channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-//            System.out.println(" [x] Sent '" + message + "'");
-//        }
+
 
 
         MQResponse response = new MQResponse();
@@ -79,35 +45,6 @@ public class RabbitmqController {
         response.setMessage("Message Send Success!");
         response.setData("Message Send Success!");
         return new ResponseEntity<>(response, headers, HttpStatus.OK);
-//
-//
-//        try {
-//            ConnectionFactory factory = new ConnectionFactory();
-//            factory.setHost(host);
-//            factory.setPort(port);
-//            factory.setUsername(username);
-//            factory.setPassword(password);
-//
-//
-//
-//            Connection connection = factory.newConnection();
-//            Channel channel = connection.createChannel();
-//            //channel.exchangeDeclare(EXCHANGE_NAME, "topic",true);
-//            //TODO 라우트 키에 해당하는 메세지 큐를 바인딩 해준다.
-//            channel.queueBind("send.test",EXCHANGE_NAME,routingKey);
-//
-//            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
-//            System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
-//
-//
-//
-//        response.setStatus(StatusEnum.OK);
-//        response.setMessage("Message Send Success!");
-//        response.setData("Message Send Success!");
-//        System.out.println("Message sent to the RabbitMQ Topic Exchange Successfully") ;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
+
     }
 }
