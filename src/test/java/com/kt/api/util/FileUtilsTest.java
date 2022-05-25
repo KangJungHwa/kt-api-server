5e9fba24de164c00c130a93a5cecd1114b11da98
JungHwa Kang <pacino.kang@gmail.com>
2022-05-25 17:09:21 +0900

    jdbc 드라이버 mysql > mariadb로 변경

--- src/test/java/com/kt/api/util/FileUtilsTest.java
+++ src/test/java/com/kt/api/util/FileUtilsTest.java
@@ -2,7 +2,8 @@ package com.kt.api.util;
 
 public class FileUtilsTest {
     public static void main(String args[]) {
-        FileUtils.delete("C:\\service-api\\upload\\upload_test");
+        System.out.println(EncryptionUtils.getDecodingStr("cGFzc3dvcmQ="));
+        //FileUtils.delete("C:\\service-api\\upload\\upload_test");
         //FileUtils.readFile("C:\\service-api\\upload\\upload_test");
     }
 }
